937763b8cd9de42c426f2c7a013e260e1841156b
Harun Abdi <harunrush9@gmail.com>
2019-04-20 00:56:23 -0500

    final

--- bamazonC.js
+++ bamazonC.js
@@ -1,7 +1,7 @@
 //importing mysql from the package
 var mysql = require("mysql");
 var inquirer = require('inquirer');
-var products = require("cli-products");
+
 
 // This are object variable which are used for connecting to mySQl database
 var connection = mysql.createConnection({
@@ -27,8 +27,67 @@ function showTable() {
             console.log(results[i].itemID + " || " + results[i].product_name + " || " +
                 results[i].department_name + " || " + results[i].price + " || " + results[i].stock_quantity + "\n");
         }
+        askCustomer(results)
     });
 }
+
+var askCustomer = function (results) {
+    inquirer.prompt([{
+        type: "input",
+        name: "choice",
+        message: "what kind of product do you want to purchase today?"
+
+    }
+
+    ])
+        .then(function (answer) {
+            var correct = false;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].product_name === answer.choice) {
+                    correct = true;
+                    var product = answer.choice;
+                    var id = i;
+
+                    inquirer.prompt({
+
+                        type: "input",
+                        name: "amount",
+                        message: "How many  of this product will you like to purchase?"
+                    })
+                        .then(function (answer) {
+                            if ((results[id].stock_quantity - answer.amount) > 0) {
+
+
+                                connection.query("UPDATE products SET stockQuantity=" + (results[id].stock_quantity - answer.amount) + "' WHERE productName='" + product + "'", function (error, results) {
+                                    console.log(" Thank you just purchased Item!");
+                                    showTable();
+                                })
+                            } else {
+                                console.log("the product is not available");
+                                askCustomer(results);
+                            }
+
+
+
+
+                        })
+                }
+            }
+        })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
